2274c6c5f18fd13d9eb3c2d04e0f97669132fe7d
Izbassar Nurlan <190103262@stu.sdu.edu.kz>
2024-02-17 20:24:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+function translate(languageCode) {
+  const currentTranslation = translations[languageCode];
+
+  document.getElementById('intro-name').textContent =
+    currentTranslation.myNameIs;
+  document.getElementById('intro-name-span').textContent =
+    currentTranslation.myName;
+  document.getElementById('intro-title').textContent =
+    currentTranslation.myTitle;
+  document.getElementById('intro-student').textContent =
+    currentTranslation.student;
+  document.getElementById('intro-sdu').textContent =
+    currentTranslation.studying;
+  document.getElementById('nav-projects').textContent =
+    currentTranslation.projects;
+  document.getElementById('nav-about').textContent = currentTranslation.about;
+  document.getElementById('nav-contact').textContent =
+    currentTranslation.contact;
+  document.getElementById('nav-resume').textContent = currentTranslation.resume;
+  document.getElementById('proud').textContent = currentTranslation.proud;
+  document.getElementById('latest-project-title').textContent =
+    currentTranslation.latestProject;
+  document.getElementById('minimusic-desc').textContent =
+    currentTranslation.miniMusicDesc;
+  document.getElementById('diploma-project-title').textContent =
+    currentTranslation.diplomaProject;
+  document.getElementById('kitaptap-desc').textContent =
+    currentTranslation.kitaptapDesc;
+  document.getElementById('contact-message').textContent =
+    currentTranslation.contactMessage;
+  document.getElementById('technologies').textContent =
+    currentTranslation.technologies;
+  document.getElementById('technologies2').textContent =
+    currentTranslation.technologies;
+  document.getElementById('contact-me').textContent =
+    currentTranslation.contactme;
+  document.getElementById('email-me').textContent = currentTranslation.emailme;
+  document.getElementById('izba').innerHTML = currentTranslation.izba;
+  document.getElementById('rights').innerHTML = currentTranslation.rights;
+
+  const flagImage = document.getElementById('current-flag');
+  if (languageCode === 'en') {
+    flagImage.src = 'img/Flag_of_Great_Britain_(English_version).png';
+    flagImage.alt = 'USA Flag';
+  } else if (languageCode === 'ru') {
+    flagImage.src = 'img/Flag_of_Russia.png';
+    flagImage.alt = 'Russian Flag';
+  }
+}
+
+const languageSwitcher = document.getElementById('language-switcher');
+languageSwitcher.addEventListener('click', function () {
+  const currentFlagSrc = document.getElementById('current-flag').src;
+  let newLanguage = 'ru';
+  if (
+    currentFlagSrc.includes('img/Flag_of_Great_Britain_(English_version).png')
+  ) {
+    newLanguage = 'ru';
+  } else {
+    newLanguage = 'en';
+  }
+
+  translate(newLanguage);
+});
+
+const defaultLanguage = 'ru';
+translate(defaultLanguage);
---
+++ js/translations.js
@@ -0,0 +1,54 @@
+const translations = {
+  en: {
+    myNameIs: 'Hello, my name is ',
+    myName: 'Izbassar Nurlan',
+    myTitle: 'I develop the front end for websites and mobile applications.',
+    student: "I'm a student and developer who specializes in React.",
+    studying: 'I am now studying at SDU.',
+    projects: 'Projects',
+    about: 'About',
+    contact: 'Contact',
+    resume: 'Resume',
+    sdu: 'I am now studying at SDU.',
+    proud: "Projects I'm proud of",
+    latestProject: 'Latest Project',
+    miniMusicDesc:
+      'MiniMusic is a lightweight, responsive web app for playing your favorite tunes. Enjoy a clean interface, intuitive controls, and smooth playback experience on any device. Built with React and @mui/material, MiniMusic prioritizes simplicity and ease of use while offering essential music player features.',
+    diplomaProject: 'My Diploma Project',
+    kitaptapDesc:
+      'Book Crossing Almaty is a platform for book lovers and students in Almaty. This project aims to make book sharing and discovery easy and fun.',
+    contactMessage:
+      "I'm currently seeking opportunities in front-end web and mobile development using React. If you have any exciting projects or job opportunities, feel free to reach out to me.",
+    technologies: 'Technologies used:',
+    contactme: 'Contact me',
+    emailme: 'Email me',
+    izba: 'Izbassar Nurlan &middot; Developer',
+    rights: '&copy; 2024 Izbassar Nurlan. All rights reserved.',
+  },
+  ru: {
+    myNameIs: 'Привет, меня зовут ',
+    myName: 'Избасар Нурлан',
+    myTitle: 'Я разрабатываю интерфейсы для веб-сайтов и мобильных приложений.',
+    student: 'Я студент и разработчик, специализирующийся на React.',
+    studying: 'В настоящее время я учусь в СДУ.',
+    projects: 'Проекты',
+    about: 'Обо мне',
+    contact: 'Контакты',
+    resume: 'Резюме',
+    sdu: 'В настоящее время я учусь в СДУ.',
+    proud: 'Проекты, которыми я горжусь',
+    latestProject: 'Последний проект',
+    miniMusicDesc:
+      'MiniMusic — это легкое, адаптивное веб-приложение для воспроизведения ваших любимых мелодий. Наслаждайтесь чистым интерфейсом, интуитивно понятным управлением и плавным воспроизведением на любом устройстве. MiniMusic, созданный с помощью React и @mui/material, отдает приоритет простоте и удобству использования, предлагая при этом основные функции музыкального проигрывателя.',
+    diplomaProject: 'Мой дипломный проект',
+    kitaptapDesc:
+      'Book Crossing Almaty - это платформа для любителей книг и студентов в Алматы. Этот проект направлен на то, чтобы сделать обмен книгами и их поиск простым и увлекательным.',
+    contactMessage:
+      'В настоящее время я ищу возможности в области фронтенд-разработки веб-сайтов и мобильных приложений с использованием React. Если у вас есть интересные проекты или вакансии, не стесняйтесь обращаться ко мне.',
+    technologies: 'Используемые технологии:',
+    contactme: 'Связаться со мной',
+    emailme: 'Напишите мне',
+    izba: 'Избасар Нурлан &middot; Разработчик',
+    rights: '&copy; 2024 Избасар Нурлан. Все права защищены.',
+  },
+};
